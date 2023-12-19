5213d77c771318aab01ec0b3e722ec0e21afea00
Samiul <samiultalukder@gmail.com>
2023-12-20 00:40:26 +0600

    Find Course and Update and Deleting course api added

--- index.js
+++ index.js
@@ -22,14 +22,14 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const xcourses = client.db("todoapp").collection("xcourses");
+    const coursesCollection = client.db("todoapp").collection("xcourses");
 
     // creating a course
 
     app.post("/api/course/add", async (req, res) => {
       const newCourse = req.body;
       console.log(newCourse);
-      const result = await xcourses.insertOne(newCourse);
+      const result = await coursesCollection.insertOne(newCourse);
       res.send("The course has been added successfully");
     });
 
@@ -37,9 +37,52 @@ async function run() {
 
     app.get("/api/course/allcourses", async (req, res) => {
       const query = {};
-      const cursor = xcourses.find(query);
+      const cursor = coursesCollection.find(query);
       const allCourses = await cursor.toArray();
       res.send(allCourses);
+    });
+
+    // Find Course By ID
+
+    app.get("/api/course/findbyid/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const course = await coursesCollection.findOne(query);
+      res.send(course);
+    });
+
+    //Deleting a course
+
+    app.delete("/api/course/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coursesCollection.deleteOne(query);
+      res.send("Course Deleted" + result);
+    });
+
+    // updating a course
+
+    app.put("/api/course/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateCourseDetails = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = {
+        upsert: true,
+      };
+      const updateDoc = {
+        $set: {
+          name: updateCourseDetails.nameUpdate,
+          description: updateCourseDetails.descriptionUpdate,
+          price: updateCourseDetails.priceUpdate,
+          duration: updateCourseDetails.durationUpdate,
+        },
+      };
+      const result = await coursesCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
     });
   } finally {
   }
